62075d986a21e964c2b9c71a01f7547b35ae6899
Ducas Francis <ducas@ducasfrancis.com>
2016-02-23 22:13:20 +1100

    change install order and include python-software-properties for add-apt-repository

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -2,11 +2,16 @@
 #
 # Setup the the box. This runs as root
 
+
+apt-get -y update
+
+apt-get -y install curl python-software-properties
+
 wget -qO - http://packages.confluent.io/deb/2.0/archive.key | apt-key add -
 
 add-apt-repository "deb http://packages.confluent.io/deb/2.0 stable main"
 
 apt-get -y update
 
-apt-get -y --force-yes install curl openjdk-7-jdk confluent-platform-2.11.7
+apt-get -y install --force-yes openjdk-7-jdk confluent-platform-2.11.7
 
