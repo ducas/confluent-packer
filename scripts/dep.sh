3ede840828cfd0c4674fa2f7d7d7895e72d5c9a7
Ducas Francis <ducas@ducasfrancis.com>
2016-02-28 15:40:29 +1100

    fix issue with ubuntu + confluent repo

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -2,16 +2,14 @@
 #
 # Setup the the box. This runs as root
 
-
 apt-get -y update
 
 apt-get -y install curl python-software-properties
 
 wget -qO - http://packages.confluent.io/deb/1.0/archive.key | apt-key add -
 
-add-apt-repository "deb [arch=all] http://packages.confluent.io/deb/1.0 stable main"
+sh -c "echo deb [arch=all] http://packages.confluent.io/deb/1.0 stable main >> /etc/apt/sources.list"
 
 apt-get -y update
 
-apt-get install confluent-platform-2.10.4
-
+apt-get install -y confluent-platform-2.10.4
